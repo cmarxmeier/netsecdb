9d5e8e39dda9457fde2d5a3fb5865e06a759085b
Claus Marxmeier <claus@marxmeier.de>
2025-04-04 12:04:55 +0200

    Update netsecdb.py

--- query/netsecdb.py
+++ query/netsecdb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+# network security query script in python qt running on linux 64bit
+# (c) 2025 by alladin@routeme.de
 
 import sys
 from PyQt5 import QtWidgets, QtGui, QtSql, QtCore
